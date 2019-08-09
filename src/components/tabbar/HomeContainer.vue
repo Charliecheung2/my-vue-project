3f96750c443df11574bc607d7f6739e173d30105
Charliecheung2 <84767967@qq.com>
2019-08-09 22:11:48 +0800

    8月9号晚提交的更改(第二次)

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -7,27 +7,27 @@
         </mt-swipe>
         <ul class="mui-table-view mui-grid-view mui-grid-9">
             <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-                <span class="mui-icon mui-icon-home"></span>
+                <img src="../../images/menu1.png" alt="">
                 <div class="mui-media-body">新闻资讯</div>
             </a></li>
             <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-                <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+                <img src="../../images/menu2.png" alt="">
                 <div class="mui-media-body">图片分享</div>
             </a></li>
             <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-                <span class="mui-icon mui-icon-chatbubble"></span>
+                <img src="../../images/menu3.png" alt="">
                 <div class="mui-media-body">商品购买</div>
             </a></li>
             <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-                <span class="mui-icon mui-icon-location"></span>
+                <img src="../../images/menu4.png" alt="">
                 <div class="mui-media-body">留言反馈</div>
             </a></li>
             <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-                <span class="mui-icon mui-icon-search"></span>
+                <img src="../../images/menu5.png" alt="">
                 <div class="mui-media-body">视频专区</div>
             </a></li>
             <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
-                <span class="mui-icon mui-icon-phone"></span>
+                <img src="../../images/menu6.png" alt="">
                 <div class="mui-media-body">联系我们</div>
             </a></li>
         </ul>
@@ -49,10 +49,10 @@
         methods: {
             getSwipe() {
                 this.$http.get('http://120.77.181.41:3000/api/getcover').then(result => {
-                    console.log(result.body.imgs);
+                    // console.log(result.body.imgs);
                     if (result.body.status === 1) {
                         this.swipeList = result.body.imgs;
-                        Toast("加载轮播图成功");
+                        // Toast("加载轮播图成功");
                     } else {
                         Toast("加载轮播图失败");
                     }
@@ -88,6 +88,13 @@
     .mui-grid-view.mui-grid-9{
         background-color: #ffffff;
         border: none;
+        img{
+            width: 60px;
+            height: 60px;
+        }
+        .mui-media-body{
+            font-size: 13px;
+        }
     }
     .mui-grid-view.mui-grid-9 .mui-table-view-cell{
         border: 0;
