a6356f9117aef555cb9e550e25573801e21bb8b4
Charliecheung2 <84767967@qq.com>
2019-08-09 21:07:56 +0800

    8月9号晚提交的更改

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <mt-swipe :auto="4000" :show-indicators="false">
+            <mt-swipe-item v-for="item in swipeList" :key="item">
+                <img :src="item" alt="">
+            </mt-swipe-item>
+        </mt-swipe>
+        <ul class="mui-table-view mui-grid-view mui-grid-9">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-home"></span>
+                <div class="mui-media-body">新闻资讯</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+                <div class="mui-media-body">图片分享</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-chatbubble"></span>
+                <div class="mui-media-body">商品购买</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-location"></span>
+                <div class="mui-media-body">留言反馈</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-search"></span>
+                <div class="mui-media-body">视频专区</div>
+            </a></li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <span class="mui-icon mui-icon-phone"></span>
+                <div class="mui-media-body">联系我们</div>
+            </a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import {Toast} from 'mint-ui'
+
+    export default {
+        data() {
+            return {
+                swipeList: []
+            }
+        },
+        created() {
+            this.getSwipe();
+        },
+        methods: {
+            getSwipe() {
+                this.$http.get('http://120.77.181.41:3000/api/getcover').then(result => {
+                    console.log(result.body.imgs);
+                    if (result.body.status === 1) {
+                        this.swipeList = result.body.imgs;
+                        Toast("加载轮播图成功");
+                    } else {
+                        Toast("加载轮播图失败");
+                    }
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .mint-swipe {
+        height: 200px;
+
+        .mint-swipe-item {
+            &:nth-child(1) {
+                background-color: #0062cc;
+            }
+
+            &:nth-child(2) {
+                background-color: red;
+            }
+
+            &:nth-child(3) {
+                background-color: blue;
+            }
+
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+    .mui-grid-view.mui-grid-9{
+        background-color: #ffffff;
+        border: none;
+    }
+    .mui-grid-view.mui-grid-9 .mui-table-view-cell{
+        border: 0;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 //入口文件
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
 import './lib/mui/css/mui.min.css'
-import { Header, Tabbar, TabItem} from 'mint-ui'
-Vue.component(Header.name, Header)
-Vue.component(Tabbar.name, Tabbar)
-Vue.component(TabItem.name, TabItem)
+import './lib/mui/css/icons-extra.css'
+import { Header, Tabbar, TabItem, Swipe, SwipeItem} from 'mint-ui'
+import router from './router.js'
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
+
+Vue.component(Header.name, Header);
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
 import app from './App.vue'
 var vm=new Vue({
   el:'#app',
-  render:c=>c(app)
-})
+  render:c=>c(app),
+  router
+});
