c57310dbd44c2c117a22748e70710fe53b9e291a
Charliecheung2 <84767967@qq.com>
2019-08-30 21:43:36 +0800

    8月30号，完结撒花

--- src/components/subcomponents/Swiper.vue
+++ src/components/subcomponents/Swiper.vue
@@ -10,7 +10,7 @@
 
 <script>
     export default {
-        props:["swipeList", "isfall"],
+        props:["swipeList", "isfull"],
         data(){
             return{
             }
--- src/components/tabbar/CartContainer.vue
+++ src/components/tabbar/CartContainer.vue
@@ -22,8 +22,13 @@
         <div class="goods-list">
             <div class="mui-card">
                 <div class="mui-card-content">
-                    <div class="mui-card-content-inner">
-                        这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等
+                    <div class="mui-card-content-inner jiesuan">
+                        <div class="left">
+                            <p>总计（不含运费）</p>
+                            <p>已勾选商品 0 件， 总价¥0</p>
+                        </div>
+                        <mt-button type="danger">结算</mt-button>
+
                     </div>
                 </div>
             </div>
@@ -32,39 +37,56 @@
 </template>
 
 <script>
-import numbox from "../subcomponents/cart_numberbox.vue"
+    import numbox from "../subcomponents/cart_numberbox.vue"
+
     export default {
-    components:{
-        numbox
-    }
+        data() {
+            return {
+                cart: []
+            }
+        },
+        methods: {},
+        components: {
+            numbox
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-    .cart-container{
+    .cart-container {
         background-color: #eee;
         overflow: hidden;
-        .goods-list{
-            .mui-card-content-inner{
+
+        .goods-list {
+            .mui-card-content-inner {
                 display: flex;
                 align-items: center;
             }
-            img{
-                width:60px;
+
+            img {
+                width: 60px;
                 height: 60px;
             }
-            h1{
+
+            h1 {
                 font-size: 13px;
             }
-            .info{
+
+            .info {
                 display: flex;
                 flex-direction: column;
                 justify-content: space-between;
-                .price{
+
+                .price {
                     color: red;
                     font-weight: bold;
                 }
             }
         }
+    }
+    .jiesuan{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
 </style>
